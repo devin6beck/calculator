d50d2735f82351c214c240d40fcfe2813ff595e3
Devin Beck <devin6beck@gmail.com>
2022-02-10 13:34:48 -0700

    messy - can only calculat 1 digit values

---
+++ script.js
@@ -0,0 +1,110 @@
+const screen = document.getElementById("screen");
+const buttons = document.getElementById("buttons");
+const digitButtons = document.getElementById("digitButtons");
+const operateButtons = document.getElementById("operateButtons");
+const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equals");
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+let sum = "";
+
+
+start();
+
+function start() {
+  digitButtons.addEventListener("click", (e) => {
+    screen.textContent = e.target.id;
+    
+    if (num1 === "") {
+      num1 = e.target.id
+    } else {
+      num2 = e.target.id
+    }
+  })
+
+  operateButtons.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "add": operator = "+";
+        break;
+      case "subtract": operator = "-";
+        break;
+      case "multiply": operator = "*";
+        break;
+      case "divide": operator = "/";
+    }
+  })
+
+  equalsButton.addEventListener("click", (e) => {
+    screen.textContent = operate(operator, num1, num2);
+  })
+
+  clearButton.addEventListener("click", (e) => {
+    screen.textContent = ""
+    num1 = "";
+    num2 = "";
+    operator = "";
+    sum = "";
+  })
+}
+
+
+
+// function start() {
+//   let num1 = "";
+//   let num2 = "";
+//   let sum = "";
+
+//   digitButtons.addEventListener("click", (e) => {
+//     console.log(`e.target.id: ${e.target.id}`)
+//     console.log(`e.target.nodeName: ${e.target.nodeName}`)
+//     screen.textContent = e.target.id;
+    
+//     if (num1 === "") {
+//       num1 = e.target.id
+//     } else {
+//       num2 = e.target.id
+//       screen.textContent = add(parseInt(num1), parseInt(num2));
+//       num1 = num2;
+//     }
+//     console.log(`num1: ${parseInt(num1)}`)
+//     console.log(`num2: ${num2}`)
+
+//   })
+
+
+// }
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  if (n2 === 0) {
+    return "Cannot divide by 0..."
+  }
+  return n1 / n2;
+}
+
+function operate(operator, n1, n2) {
+  switch(operator) {
+    case "+" : return add(parseInt(n1), parseInt(n2));
+    case "-" : return subtract(parseInt(n1), parseInt(n2));
+    case "/" : return divide(parseInt(n1), parseInt(n2));
+    case "*" : return multiply(parseInt(n1), parseInt(n2));
+  }
+}
+
+// console.log(operate("/", 2, 4))
+
+// const screen = document.getElementById("screen");
+// screen.textContent = "Hi"
